6c60f5a0b6aaa2aebc45eaab397d917bfc068cd6
刘丽娜 <13614477385@163.com>
2019-07-05 13:49:31 +0800

    路由

--- App.js
+++ App.js
@@ -4,26 +4,30 @@ import AppContainer from './AppContainer';
 import router from './src/router/Routers'
 
 
-class App extends React.Component {
+export default class App extends React.Component {
     render() {
-        return <AppContainer/>
+        return <AppRouter/>
     }
 }
 
-const AppNavigator = createStackNavigator(router[0],{
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-            fontWeight: 'bold',
-        },
-        navigationOptions: {
-            tabBarLabel: 'Home!',
-        },
+const AppNavigator = createStackNavigator(
+    {
+        ...router[0]
     },
+    {
+        initialRouteName: "Home",
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+            navigationOptions: {
+                tabBarLabel: 'Home!',
+            },
+        },
 });
-const ss = createStackNavigator(router[1]);
-export default App=createAppContainer(AppNavigator);
+
+const AppRouter=createAppContainer(AppNavigator);
